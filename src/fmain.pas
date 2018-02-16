12db21abce5fa04f0d9abb534210e9de3afa45c1
ZAwuza <andreizawuza@gmail.com>
2018-02-16 22:31:19 +0100

    Added Lazarus project

---
+++ src/fmain.pas
@@ -0,0 +1,38 @@
+unit fMain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage('Built with Blue Ocean!');
+end;
+
+end.
+
