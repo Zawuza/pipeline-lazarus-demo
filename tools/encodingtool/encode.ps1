0f765d2bfa29992ce82c64a3ff9c10742de61fee
ZAwuza <andreizawuza@gmail.com>
2018-02-17 20:58:46 +0100

    encode script fix

--- tools/encodingtool/encode.ps1
+++ tools/encodingtool/encode.ps1
@@ -1,3 +1,3 @@
 $filename = (Get-Location).Path + "\results2.xml"
-$content = Get-Content results_test.xml -Encoding Unicode 
+$content = Get-Content results.xml -Encoding Unicode 
 [IO.File]::WriteAllLines($filename, $content)
